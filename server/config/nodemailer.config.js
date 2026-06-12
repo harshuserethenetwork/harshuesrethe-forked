1d4d9e59e08052d8cf2223fb771e3d92b2e6d78d
Harsh Userethe <82079150+HarshUserethe@users.noreply.github.com>
2026-06-12 21:13:27 +0530

    Forced mailer to use ipv4

--- server/config/nodemailer.config.js
+++ server/config/nodemailer.config.js
@@ -5,6 +5,7 @@ const transporter = nodemailer.createTransport({
   host: env.smtp.host,
   port: env.smtp.port,
   secure: env.smtp.port === 465,   // true for 465, false for 587
+  family: 4, // Force IPv4
   auth: {
     user: env.smtp.user,
     pass: env.smtp.pass,
