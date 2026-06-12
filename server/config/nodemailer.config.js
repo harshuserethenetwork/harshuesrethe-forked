1db824d59cf02b10a1fd66b2620de74e138c3726
Harsh Userethe <82079150+HarshUserethe@users.noreply.github.com>
2026-06-12 22:13:21 +0530

    Update nodemailer.config.js

--- server/config/nodemailer.config.js
+++ server/config/nodemailer.config.js
@@ -5,10 +5,6 @@ const transporter = nodemailer.createTransport({
   host: env.smtp.host,
   port: env.smtp.port,
   secure: env.smtp.port === 465,   // true for 465, false for 587
-  family: 4, // Force IPv4
-  connectionTimeout: 30000,
-  greetingTimeout: 30000,
-  socketTimeout: 30000,
   auth: {
     user: env.smtp.user,
     pass: env.smtp.pass,
